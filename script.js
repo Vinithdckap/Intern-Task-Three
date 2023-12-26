268abe62e672c453a54375882b7407ecf6e0ec03
Vinith Krishnan <vinithkdckap@gmail.com>
2023-12-26 16:00:39 +0530

    footer collapse finished

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 
+// foote collapse function
+
+$(".add").click(function () {
+    $(this).next('.footer-lists').slideToggle(700)
+    $('.footer-lists').not($(this).next('.footer-lists')).slideUp(1000);
 
 
-let toggle = document.querySelector("#menu-icon");
+    if ($(this)[0].innerText == "add") {
+        $(this).text("remove");
+    }
+    else {
+        $(this).text("add")
+    }
+})
 
 
-toggle.addEventListener('click', () => {
-
-    
-
-
-    
-  });
